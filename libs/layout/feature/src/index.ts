867ab0af84671a85a39466746659d2af085e5505
youurt <ugurtigu@gmail.com>
2023-03-29 00:10:06 +0200

    - add feature layout lib - add rules for scope of this lib

---
+++ libs/layout/feature/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/layout-feature/layout-feature.component';
+
+export * from './lib/lib.routes';
---
+++ libs/layout/feature/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureComponent } from './layout-feature/layout-feature.component';
+
+export const layoutFeatureRoutes: Route[] = [
+  { path: '', component: FeatureComponent },
+];
