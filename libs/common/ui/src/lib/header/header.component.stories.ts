e911e6dc394633c75bdc2aa5b36ea7a377a2ed84
youurt <ugurtigu@gmail.com>
2023-03-29 01:12:34 +0200

    - add storybook for common ui

---
+++ libs/common/ui/src/lib/header/header.component.stories.ts
@@ -0,0 +1,15 @@
+import { Meta } from '@storybook/angular';
+import { HeaderComponent } from './header.component';
+
+export default {
+  title: 'HeaderComponent',
+  component: HeaderComponent
+} as Meta<HeaderComponent>;
+
+export const Primary = {
+  render: (args: HeaderComponent) => ({
+    props: args,
+  }),
+  args: {
+  },
+};
