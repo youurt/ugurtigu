97d5410cd2c01d356adb594bd99360462c7e2cbd
youurt <ugurtigu@gmail.com>
2023-03-30 00:08:05 +0200

    - add style for header

--- libs/common/ui/src/lib/common-ui-header/common-ui-header.component.ts
+++ libs/common/ui/src/lib/common-ui-header/common-ui-header.component.ts
@@ -1,18 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CommonUiLogoComponent } from '../common-ui-logo/common-ui-logo.component';
 
 @Component({
   selector: 'ugurtigu-common-ui-header',
   standalone: true,
-  imports: [CommonModule, CommonUiLogoComponent],
+  imports: [
+    CommonModule,
+    CommonUiLogoComponent,
+    RouterModule,
+    RouterTestingModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./common-ui-header.component.scss'],
   template: `<header
     class="py-6 sticky top-0 z-10 bg-ugurtigu-base text-ugurtigu-contrast"
   >
-    <nav class="max-w-[1280px] mx-auto flex justify-between px-4">
-      <ugurtigu-common-ui-logo></ugurtigu-common-ui-logo> Header
+    <nav
+      class="max-w-[1280px] mx-auto flex justify-between px-4 items-center flex-col gap-4 sm:flex-row sm:gap-0"
+    >
+      <a [routerLink]="['/']">
+        <ugurtigu-common-ui-logo></ugurtigu-common-ui-logo>
+      </a>
+      <div class="justify-end align-center gap-8 flex">
+        <a
+          class="c-common-ui-header__link"
+          routerLinkActive="active"
+          *ngFor="
+            let item of [
+              { name: 'Home', link: '/', exact: true },
+              { name: 'Work', link: '/work', exact: false },
+              { name: 'Writing', link: '/writing', exact: false },
+              { name: 'Contact', link: '/contact', exact: false }
+            ]
+          "
+          [routerLink]="[item.link]"
+          [routerLinkActiveOptions]="{ exact: item.exact }"
+        >
+          {{ item.name }}
+        </a>
+      </div>
     </nav>
   </header> `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommonUiHeaderComponent {}
+export class CommonUiHeaderComponent {
+  @HostBinding('class.c-common-ui-header') hostClass = true;
+}
